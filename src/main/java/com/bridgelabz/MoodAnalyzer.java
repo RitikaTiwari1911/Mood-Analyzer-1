3d795f01285a7058364aca58cb971aa16a421475
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-21 20:33:02 +0530

    REFACTORED: Code will take the mood message in constructor

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,7 +1,23 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-        public String analyzeMood(String message) {
+
+    private String message;
+    // Refactor the code to take the mood message in the constructor
+    public MoodAnalyzer(){
+            this.message=message;
+    }
+
+    public MoodAnalyzer(String message){
+        this.message=message;
+
+    }
+
+    public String analyzeMood(String message){
+        return analyzeMood();
+    }
+
+        public String analyzeMood() {
         if (message.contains("sad"))
             return "SAD";
         else
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -6,18 +6,19 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
 
     //Test Case 1.1
+    //Message is being taken in parameter
     @Test
     public void givenMessage_Should_ReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyzeMood("I am in a sad mood");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in a sad mood");
+        String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("SAD", mood);
     }
 
     //Test Case 1.2
     @Test
     public void givenMessage_Should_ReturnHappy(){
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyzeMood("I am in any mood");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
+        String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("HAPPY",mood);
     }
 }
