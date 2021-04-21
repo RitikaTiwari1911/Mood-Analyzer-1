3002fa09e85e3804d222abd65c454324b0247dc3
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-21 20:00:52 +0530

    ADDED: TC 1.1-given method should retun SAD

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -5,21 +5,12 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
 
+    //Test Case 1.1
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() {
+    public void givenMessage_Should_ReturnSad() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyzeMood("This is a sad message");
-        Assert.assertEquals("HAPPY",mood);
-
+        String mood = moodAnalyzer.analyzeMood("I am in a sad mood");
+        Assert.assertEquals("SAD", mood);
     }
-
-    @Test
-    public void givenMessage_WhenNotSad_ShouldReturnHappy(){
-        MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
-        String mood= moodAnalyzer.analyzeMood("This is a Happy message");
-        Assert.assertEquals("HAPPY",mood);
-    }
-
-
-
 }
+
